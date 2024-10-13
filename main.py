376c4edb04fbb0b8e1aa6636028b1eaf487b6a28
AntoinePerrin25 <antoine.perrin1@utbm.fr>
2024-10-13 18:06:17 +0200

    Initialization, Classes

---
+++ main.py
@@ -0,0 +1,79 @@
+class Piece:
+    def __init__(self, couleur, forme):
+        self.couleur = couleur
+        self.forme=forme
+
+    def rotate(self):
+        """Rotate the piece 90 degrees clockwise."""
+        self.forme = [list(row) for row in zip(*self.forme[::-1])]
+    def all_rotations(self):
+        rotations = []
+        current_forme = self.forme
+        for _ in range(4):
+            current_forme = [list(row) for row in zip(*current_forme[::-1])]
+            rotations.append(current_forme)
+        return rotations
+    
+    
+    def display(self):
+        '''Display the piece'''
+
+
+        
+class Plateau:
+    def __init__(self, taille):
+        self.taille = taille
+        
+        
+def initialiser() -> list:
+    red = Piece("red",
+    [[1, 1, 1, 1],
+     [0, 0, 0, 1]])
+    orange = Piece("orange",
+    [[0, 1, 0],
+    [1, 1, 1],
+    [1, 0, 0]])
+    yellow = Piece("yellow",
+    [[1, 1, 1, 1],
+     [0, 1, 0, 0]])
+    lime = Piece("lime",
+    [[1, 1, 1],
+     [1, 0, 1]])
+    green = Piece("green",
+    [[1, 1, 1],
+     [0, 1, 0]])
+    white = Piece("white",
+    [[1, 1, 1],
+     [0, 1, 1]])
+    cyan = Piece("cyan",
+    [[0, 1],
+    [1, 1]])
+    cyanblue = Piece("cyanblue",
+    [[1, 1, 1],
+    [1, 0, 0],
+    [1, 0, 0]])
+    blue = Piece("blue",
+    [[0, 0, 1],
+     [1, 1, 1]])
+    purple = Piece("purple",
+    [[1, 1, 0],
+     [0, 1, 1],
+     [0, 0, 1]])
+    darkred = Piece("darkred",
+    [[0, 1, 1],
+     [1, 1, 0]])
+    pink = Piece("pink",
+    [[1, 1, 0, 0],
+     [0, 1, 1, 1]])
+    return [red, orange, yellow, lime, green, white, cyan, cyanblue, blue, purple, darkred, pink]
+
+def main():
+    Pieces = initialiser()
+    for piece in Pieces:
+        piece.display()
+        print(piece.all_transformations())
+        print()
+    
+    
+if __name__ == "__main__":
+    main()
